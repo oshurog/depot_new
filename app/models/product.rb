47fe61db4338b50b95ff83cacfddf34cee327e14
oshurog <oleg.surogin@outlook.com>
2012-08-13 20:59:46 +0300

    cart creation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 
-	default_scope :order => 'title'
+  default_scope :order => 'title'
   
   attr_accessible :description, :image_url, :price, :title
   
@@ -9,5 +9,20 @@ class Product < ActiveRecord::Base
   validates :title, :uniqueness => true
 
   ##number_to_currency :price, :locale => 'ee', :precision => 2
+
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+  	if line_items.empty?
+  		return true
+  	else
+  		errors.add(:base, 'Line Items present')
+  		return false
+  	end
+  end
 
 end
